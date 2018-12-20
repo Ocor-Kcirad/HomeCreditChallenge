fe0537024f96eb3f398d2b80c58a66932433eff4
Darick Roco <darick@creativehothouse.com>
2018-12-20 21:03:10 +0800

    Apply delay on splash activity

--- app/src/main/java/com/hello/homecreditchallenge/feature/splash/SplashActivity.kt
+++ app/src/main/java/com/hello/homecreditchallenge/feature/splash/SplashActivity.kt
@@ -1,5 +1,30 @@
 package com.hello.homecreditchallenge.feature.splash
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hello.homecreditchallenge.R
+import com.hello.homecreditchallenge.feature.weatherlist.WeatherListActivity
+import kotlinx.coroutines.*
+import org.jetbrains.anko.intentFor
 
-class SplashActivity : AppCompatActivity()
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DELAY = 2_000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        GlobalScope.launch {
+            delay(SPLASH_DELAY)
+            withContext(Dispatchers.Main) {
+                val intent = intentFor<WeatherListActivity>()
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+}
