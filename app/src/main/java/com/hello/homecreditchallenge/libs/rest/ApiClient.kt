7dcd3c91dfdbb2b20f9b56ac05c6e409fcc854ee
Darick Roco <darick@creativehothouse.com>
2018-12-21 18:29:42 +0800

    Define open weather app id in BuildConfig

--- app/src/main/java/com/hello/homecreditchallenge/libs/rest/ApiClient.kt
+++ app/src/main/java/com/hello/homecreditchallenge/libs/rest/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.hello.homecreditchallenge.libs.rest
 
+import com.hello.homecreditchallenge.BuildConfig
 import com.hello.homecreditchallenge.model.WeatherData
 import io.reactivex.Single
 import okhttp3.OkHttpClient
@@ -11,7 +12,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
 
     private const val ENDPOINT = "http://api.openweathermap.org"
-    private const val APP_ID = "4dd9f5cfb2b4db77be919ca7e7007163"
     private const val METRIC_UNITS = "metric"
 
     private val logger =
@@ -33,9 +33,9 @@ object ApiClient {
     }
 
     fun getCitiesWeatherData(vararg cityId: String): Single<List<WeatherData>> = service
-        .getCitiesWeatherData(cityId.joinToString(separator = ","), METRIC_UNITS, APP_ID)
+        .getCitiesWeatherData(cityId.joinToString(separator = ","), METRIC_UNITS, BuildConfig.OPEN_WEATHER_APP_ID)
         .map { it.weatherList }
 
     fun getCityWeatherData(cityId: String): Single<WeatherData> = service
-        .getCityWeatherData(cityId, METRIC_UNITS, APP_ID)
+        .getCityWeatherData(cityId, METRIC_UNITS, BuildConfig.OPEN_WEATHER_APP_ID)
 }
